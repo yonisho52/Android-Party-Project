b971eb75959ad5b82526d98b129eecc34fa2a1f3
yonisho52 <yonisho52@gmail.com>
2021-02-10 20:03:35 +0200

    Retrofit functions

--- app/src/main/java/com/example/android_final_project/RetrofitInterace.java
+++ app/src/main/java/com/example/android_final_project/RetrofitInterace.java
@@ -3,6 +3,7 @@ package com.example.android_final_project;
 import com.example.android_final_project.EventsClasses.EventResult;
 
 import java.util.HashMap;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -32,8 +33,12 @@ public interface RetrofitInterace {
     Call<Void> executeAddEvent(@Body HashMap<String, String> map);
 
     //Get events request, receives existing event data
-    @POST("/getEvents")
+    @POST("/getEventsByDate")
     Call<EventResult> executeGetEvents(@Body HashMap<String, String> map);
+
+    //Get Events By Email (for place owner and dj users)
+    @GET("/getEventsByEmail")
+    Call<List<EventResult>> executeGetEventsByEmail(@Body HashMap<String, String> map);
 
     // Check if Dj Name is free
     @POST("/checkDjName")
@@ -42,6 +47,22 @@ public interface RetrofitInterace {
     // Check if place Name is free
     @POST("/checkPlaceName")
     Call<Void> executeCheckPlaceName(@Body HashMap<String, String> map);
+
+    // Get all Djs Stage Names
+    @GET("/getAllDjStageName")
+    Call<List<StageName>> getPosts();
+
+    // Get all User details by Email
+    @POST("/getUserByEmail")
+    Call<Details> getUser(@Body HashMap<String, String> map);
+
+    // Check if the party Code is free
+    @POST("/checkValidPartyCode")
+    Call<Void> checkValidPartyCode(@Body HashMap<String,String> map);
+
+    // Get Email (dj) By sending Stage Name
+    @GET("/getDjEmailByStageName")
+    Call<Details> getDjEmailByStageName(@Body HashMap<String, String> map);
 
     //Testing GET functions
 
